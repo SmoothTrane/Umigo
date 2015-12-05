862ed8978dd4b921caf248e37bde323710e086b5
Gary <gary.coltrane1@marist.edu>
2015-12-05 18:29:56 -0500

    Final push for hackathon

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
-
+var feed;
+var message;
 function main() {
     var riddles = ["Who framed __ the rabbit?", "Star ___", "Marist _____", "Marist College ___ Society"];
 
-    var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-    var message = $('#distracted');
+     feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
+    message = $('#distracted');
+
 
     if (feed.length == 0) {
         message.hide();
@@ -15,14 +16,14 @@ function main() {
             .css('font-size', '20px')
             .css('font-family', "'Helvetica Neue'sans-serif")
             .css('position', 'relative')
-            .css('top', '20px');
+            .css('top', '2px');
         $('[data-location=maincolumn]').append(message);
 // window.open("index.html");
 var divTag = document.createElement("div");
 divTag.setAttribute('id',"divy")
 
 divTag.innerHTML= '<style> position: relative </style>Fill in the blank' + '<br> ' + riddles[Math.floor(Math.random()*riddles.length)] + '</br>' + '<input id="txt" type="text"> <input id ="btn" type="Submit">';
-var txt = $("#txt");
+
 $(message).append(divTag);
 
 
@@ -41,51 +42,36 @@ $("#btn").click(function(){
 var d = document.getElementById("txt").value;
   if(d==="College"){
 
-    window.clearInterval(ap);
-
- feed.children().show();
-    $('.ticker_stream').show();
-    $('.ego_column').show();
-    $('#pagelet_games_rhc').show();
-    $('#pagelet_trending_tags_and_topics').show();
-    $('#pagelet_canvas_nav_content').show();
-    message.hide();
-
+  getRidOfThis();
     }
     else if(d==="Wars"){
-
-    window.clearInterval(ap);
-
- feed.children().show();
-    $('.ticker_stream').show();
-    $('.ego_column').show();
-    $('#pagelet_games_rhc').show();
-    $('#pagelet_trending_tags_and_topics').show();
-    $('#pagelet_canvas_nav_content').show();
-        message.hide();
+getRidOfThis();
 
 
     }
      else if(d==="Computer"){
 
    
-    window.clearInterval(ap);
-
- feed.children().show();
-    $('.ticker_stream').show();
-    $('.ego_column').show();
-    $('#pagelet_games_rhc').show();
-    $('#pagelet_trending_tags_and_topics').show();
-    $('#pagelet_canvas_nav_content').show();
-    message.hide();
+    getRidOfThis();
 
 
      }
          else if(d==="Roger"){
    
-        window.clearInterval(ap);
+getRidOfThis();
 
- feed.children().show();
+    
+
+         }
+
+
+});
+  
+} 
+
+function getRidOfThis(){
+    window.clearInterval(ap);
+    feed.children().show();
     $('.ticker_stream').show();
     $('.ego_column').show();
     $('#pagelet_games_rhc').show();
@@ -93,14 +79,10 @@ var d = document.getElementById("txt").value;
     $('#pagelet_canvas_nav_content').show();
     message.hide();
 
-    
 
-         }
-
-});
-  
-} 
+}
 var ap = window.setInterval(main, 100);
+setTimeout(getRidOfThis, 8000);
 
 
 
